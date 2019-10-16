91b1a789fbd76a0d58ff9ffc2446fb8ede8fd901
ifengzp <ifengzp@163.com>
2019-10-17 00:19:03 +0800

    功能版pdf转图片

--- renderer-process/pdf2img/js/index.js
+++ renderer-process/pdf2img/js/index.js
@@ -1,51 +1,62 @@
+/* eslint-disable */
 $(document).ready(function() {
-  let jpgBtn = new Btn("jpg");
-  let pngBtn = new Btn("png");
-  jpgBtn.disable();
-  pngBtn.disable();
-
-  $('#file-btn').on('change', function(e) {
-    const file = e.target.files[0]
+  $("#selectAll").on("click", function() {
+    const flag = $(this).get(0).checked
+    $('input:checkbox').each(function() {
+      $(this).get(0).checked = flag
+   });
+  })
+  $("#file-btn").on("change", function(e) {
+    const file = e.target.files[0];
     if (file.length === 0) {
-        alert('出错了, 文件不能为空')
-        return
+      alert("出错了, 文件不能为空");
+      return;
     }
+    $(".my-loading").show()
     let reader = new FileReader();
     reader.readAsArrayBuffer(file);
     reader.onload = function(e) {
       let typedarray = new Uint8Array(e.target.result);
       PDFJS.getDocument(typedarray).then(function(pdf) {
         if (pdf) {
-          $('#main_btn').text(file.name).css('font-size', '13px');
-          jpgBtn.clickable(function() {
-            conversion('jpg', pdf)
-          });
-          pngBtn.clickable(function() {
-            conversion('png', pdf)
-          });
+          $(".header__info-name").html(file.name)
+          $(".header__info-size").html(file.size + '字节')
+          $(".header__info-path").html(file.path)
+          $(".header__info-pages").html(pdf.numPages + "页")
+          $(".all-select-btn").show()
+          $(".header__drop").html('<div class="reset-btn"><button class="btn btn-primary" id="reset-btn" type="button">重传文件</button></div>')
+          $("#reset-btn").on("click", function() {
+            window.location.reload()
+          })
+          conversion(pdf)
         }
       });
     };
-  })
+  });
 
-  function conversion(type, pdf) {
-    let container = document.getElementById('container'),
-        pdfDoc = pdf,
-        scale = 1,
-        numPages = pdfDoc.numPages;
-    jpgBtn.ongoing();
-    pngBtn.ongoing();
+  function conversion(pdf) {
+    let container = document.getElementById("container"),
+      pdfDoc = pdf,
+      scale = 1,
+      numPages = pdfDoc.numPages;
 
     let readQueue = [];
     for (let i = 0; i < numPages; i++) {
       readQueue.push(pdfDoc.getPage(i + 1));
     }
-    Promise.all(readQueue).then((arr) => {
+    Promise.all(readQueue).then(arr => {
       let renderQueue = arr.map((item, idx) => {
-        let canvas = document.createElement('canvas');
-        let ctx = canvas.getContext('2d');
-        let viewport = item.getViewport(scale)
-        container.append(canvas);
+        let canvas = document.createElement("canvas");
+        canvas.setAttribute('class', 'img-thumbnail');
+        let ctx = canvas.getContext("2d");
+        let viewport = item.getViewport(scale);
+        let div = document.createElement("div");
+        div.setAttribute('class', 'my-content__item');
+        div.append(canvas)
+        let newDiv = document.createElement("div");
+        newDiv.innerHTML = '<div class="my-content__item-desc"><div class="checkbox"><label><input value='+idx+' checked type="checkbox"> pic-'+idx+'</label></div></div>';
+        div.append(newDiv)
+        container.append(div);
         canvas.id = idx;
         canvas.height = viewport.height;
         canvas.width = viewport.width;
@@ -53,56 +64,75 @@ $(document).ready(function() {
           canvasContext: ctx,
           viewport
         }).promise;
-      })
-      console.log('renderQueue', renderQueue)
-      let allPagesNum = renderQueue.length
-      let finishPagesNum = 0
+      });
+      console.log("renderQueue", renderQueue);
+      let allPagesNum = renderQueue.length;
+      let finishPagesNum = 0;
       renderQueue.forEach(item => {
-        item.then(res => {
-          finishPagesNum += 1
-          $('#main_btn').text(parseInt(finishPagesNum * 100 / allPagesNum) + '%').css({
-            'font-size': '20px',
-            'padding': '0 4px'
+        item
+          .then(res => {
+            finishPagesNum += 1;
+            if (finishPagesNum >= allPagesNum) {
+              $(".my-loading").delay(800).hide()
+              $("#jpg").removeAttr("disabled").removeClass("disabled").on("click", () => download("jpg"))
+              $("#png").removeAttr("disabled").removeClass("disabled").on("click", () => download("png"))
+            }
           })
-          finishPagesNum >= allPagesNum && download(type)
-        }).catch(err => {
-          finishPagesNum += 1
-          $('#main_btn').text(parseInt(finishPagesNum * 100 / allPagesNum) + '%').css({
-            'font-size': '20px',
-            'padding': '0 4px'
-          })
-          finishPagesNum >= allPagesNum && download(type)
-        })
-      })
-    })
+          .catch(err => {
+            finishPagesNum += 1;
+            if (finishPagesNum >= allPagesNum) {
+              $(".my-loading").delay(800).hide()
+              $("#jpg").removeAttr("disabled").removeClass("disabled").on("click", () => download("jpg"))
+              $("#png").removeAttr("disabled").removeClass("disabled").on("click", () => download("png"))
+            }
+          });
+      });
+    });
   }
 });
-
+let isDownloading = false;
 function download(type) {
-  let typeStr = type == png ? "image/png" : 'image/jpg'
-  console.log(type, typeStr)
+  if (isDownloading) return;
+  isDownloading = true;
+  $(".my-loading").show()
+  let typeStr = type == png ? "image/png" : "image/jpg";
   let zip = new JSZip();
   let images = zip.folder("images");
-  $('#container canvas').each(function(idx) {
-    images.file("pic-"+idx+"." + type, base64toBlob($(this)[0].toDataURL(typeStr, 0.6)), {
+  let chooseArr = []
+  $('input:checkbox:checked').each(function() {
+     const idx = +$(this).val()
+     idx > -1 && chooseArr.push(idx)
+  });
+  console.log(chooseArr)
+  $("#container canvas").each(function(idx) {
+    if (chooseArr.indexOf(idx) < 0) return
+    images.file(
+      "pic-" + idx + "." + type,
+      base64toBlob($(this)[0].toDataURL(typeStr, 0.6)),
+      {
         base64: true
-    });
-  })
-  zip.generateAsync({
-        type: "blob"
-    }).then(function(content) {
-        saveAs(content, "pdftoimages.zip");
+      }
+    );
+  });
+  zip
+    .generateAsync({
+      type: "blob"
+    })
+    .then(function(content) {
+      $(".my-loading").hide()
+      isDownloading = false;
+      saveAs(content, "pdftoimages-"+type+".zip");
     });
 }
 
-function base64toBlob(dataurl){
-  var arr = dataurl.split(',');
+function base64toBlob(dataurl) {
+  var arr = dataurl.split(",");
   var mime = arr[0].match(/:(.*?);/)[1];
   var bstr = atob(arr[1]);
   var n = bstr.length;
   var u8arr = new Uint8Array(n);
-  while(n--){
-      u8arr[n] = bstr.charCodeAt(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
   }
-  return new Blob([u8arr], {type:mime});
+  return new Blob([u8arr], { type: mime });
 }
